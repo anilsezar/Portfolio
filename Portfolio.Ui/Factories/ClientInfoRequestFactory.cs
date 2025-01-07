0f05d36e57278074614122bbb3f8ad3cb595790d
Anıl Sezer <msnanil@hotmail.com>
2025-01-07 14:21:38 +0300

    feat: NEW DESIGN: WebUI is dissolving into other layers, part 1: migration to UI layer. sln file is in here too

---
+++ Portfolio.Ui/Factories/ClientInfoRequestFactory.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.Json;
+using Portfolio.Domain.Constants;
+using Portfolio.Grpc;
+using Serilog;
+
+namespace Portfolio.Ui.Factories;
+
+public static class ClientInfoRequestFactory
+{
+    public static ClientInfoRequest Create(Dictionary<string, string> viaJavascript, IHttpContextAccessor httpContextAccessor)
+    {
+        return new ClientInfoRequest
+        {
+            Language            = GetValueOrDefault(viaJavascript, "language"),
+            Platform            = GetValueOrDefault(viaJavascript, "platform"),
+            Referrer            = GetValueOrDefault(viaJavascript, "referrer"),
+            UserAgent           = GetValueOrDefault(viaJavascript, "userAgent"),
+            DoNotTrack          = GetValueOrDefault(viaJavascript, "doNotTrack"),
+            Connection          = GetValueOrDefault(viaJavascript, "connection"),
+            Resolution          = GetValueOrDefault(viaJavascript, "resolution"),
+            DeviceMemory        = GetValueOrDefault(viaJavascript, "deviceMemory"),
+            OnLine              = GetValueOrDefault(viaJavascript, "onLine", false),
+            HardwareConcurrency = GetValueOrDefault(viaJavascript, "hardwareConcurrency"),
+            Webdriver           = GetValueOrDefault(viaJavascript, "webdriver", false),
+            CookieEnabled       = GetValueOrDefault(viaJavascript, "cookieEnabled", false),
+            MaxTouchPoints      = GetValueOrDefault(viaJavascript, "maxTouchPoints", DefaultValues.EmptyForInt),
+            IpAddress           = GetIpAddress(httpContextAccessor),
+            Extras              = GetAllRequestHeadersAsJson(httpContextAccessor)
+        };
+    }
+
+    private static string GetValueOrDefault(Dictionary<string, string> data, string key, string defaultValue = "")
+        => data.TryGetValue(key, out var value) ? value : defaultValue;
+
+    private static T GetValueOrDefault<T>(Dictionary<string, string> data, string key, T defaultValue = default)
+    {
+        if (data.TryGetValue(key, out var value) && value is not null)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch(Exception e)
+            {
+                Log.Warning(e, "Error while converting {Value} to {Type}", value, typeof(T));
+                return defaultValue; // Prevent crashes on invalid types
+            }
+        }
+        return defaultValue;
+    }
+
+    private static string GetIpAddress(IHttpContextAccessor httpContextAccessor)
+    {
+        return httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString() ?? "";
+    }
+
+    private static string GetAllRequestHeadersAsJson(IHttpContextAccessor httpContextAccessor)
+    {
+        try
+        {
+            var headers = httpContextAccessor.HttpContext?.Request.Headers;
+            return JsonSerializer.Serialize(headers);
+        }
+        catch (Exception e)
+        {
+            Log.Error("Error while serializing headers: {Error}", e.Message);
+            return "";
+        }
+    }
+}
--- Portfolio.Ui/Program.cs
+++ Portfolio.Ui/Program.cs
@@ -1,13 +1,40 @@
+using DotNetEnv;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Portfolio.Domain.Constants;
+using Portfolio.Grpc;
+using Portfolio.Infrastructure.Exceptions;
+using Portfolio.Infrastructure.Extensions;
 using Portfolio.Ui.Components;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+    Env.Load("../.env");
+EnvironmentExtensions.VerifyEnvironmentValueIsSet(EnvironmentVariableNames.Grpc_BaseUrl);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.InitLogsWithSerilog();
+builder.InitOpenTelemetry();
+builder.Services.AddHttpContextAccessor();
+
+var grpcAddress = Environment.GetEnvironmentVariable(EnvironmentVariableNames.Grpc_BaseUrl) ?? throw new MissingEnvironmentValueException();
+builder.Services.AddGrpcClient<ClientInfo.ClientInfoClient>(o =>
+{
+    o.Address = new Uri(grpcAddress);
+});
+builder.Services.AddGrpcClient<BackgroundImages.BackgroundImagesClient>(o =>
+{
+    o.Address = new Uri(grpcAddress);
+});
+
 var app = builder.Build();
 
+// todo: check this block later. Never checked it before.
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -16,8 +43,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.MapHealthChecks("/liveness", new HealthCheckOptions
+{
+    Predicate = _ => false, // Always return healthy for liveness
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK, // Liveness doesn't degrade
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
 
+app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
@@ -25,4 +62,17 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.Run();
+try
+{
+    Log.Information("UI Starting");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
+
