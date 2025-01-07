dbb2bb3fa51af0515eee375e574ea4b35fea4045
Anıl Sezer <msnanil@hotmail.com>
2025-01-07 14:51:49 +0300

    feat: NEW DESIGN: part 4: Removed IOT tables & services & tests from portfolio

--- Portfolio.Web.Api/Services/Dto/SunriseAlarmClockDirective.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portfolio.Web.Api.Services.Dto;
-
-public class SunriseAlarmClockDirective
-{
-    public List<DateTime> AlarmHours { get; set; }
-    public bool IsAlarmActive { get; set; }
-    public bool BringTheSun { get; set; }
-}
