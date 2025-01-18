48d7950f73b3a7274a32058bbedf558492a562d4
Anıl Sezer <msnanil@hotmail.com>
2025-01-18 21:11:29 +0300

    chore: Updated Globals.cs at grpc layer

--- Portfolio.Grpc/Globals.cs
+++ Portfolio.Grpc/Globals.cs
@@ -1,1 +1,5 @@
-﻿global using Grpc.Core;
+﻿global using Serilog;
+global using Grpc.Core;
+global using Portfolio.Domain.Entities;
+global using Google.Protobuf.WellKnownTypes;
+global using Portfolio.Infrastructure.Constants;
